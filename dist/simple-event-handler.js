4d42ec59ca0576cce502bd880a6d7a60ac2017df
Volodymyr Lavrynovych <volodya.lavrynovych@gmail.com>
2017-03-09 23:14:15 +0200

    v1.0.0

--- dist/simple-event-handler.js
+++ dist/simple-event-handler.js
@@ -4,8 +4,18 @@
         var subscriptions = {};
 
         this.subscribe = subscribe;
+        this.on = subscribe;
+
+        this.once = once;
+
         this.fire = fire;
+        this.emit = fire;
+
         this.unsubscribe = unsubscribe;
+        this.off = unsubscribe;
+
+        this.unsubscribeAll = unsubscribeAll;
+        this.offAll = unsubscribeAll;
 
         function subscribe(events, fn, $scope) {
             validateCallback(fn);
@@ -13,14 +23,16 @@
             if(events) {
                 if(typeof events == 'string') {
                     validateName(events);
-                    _subscribe(events);
+                    _subscribe(events, fn, $scope);
                 } else if(events instanceof Array) {
                     if(!events.length) {
                         throwNameError();
                     }
 
                     events.forEach(validateName);
-                    events.forEach(_subscribe);
+                    events.forEach(function (eventName) {
+                        _subscribe(eventName, fn, $scope);
+                    });
                 } else {
                     throwNameError();
                 }
@@ -28,17 +40,55 @@
                 throwNameError();
             }
 
-            function _subscribe(name) {
-                if (!subscriptions[name]) {
-                    subscriptions[name] = [];
-                }
+            return this;
+        }
 
-                subscriptions[name].push(fn);
-
-                $scope && $scope.$on && $scope.$on('$destroy', function () {
-                    unsubscribe(name, fn);
-                });
+        function _subscribe(name, fn, $scope) {
+            if (!subscriptions[name]) {
+                subscriptions[name] = [];
             }
+
+            subscriptions[name].push(fn);
+
+            $scope && $scope.$on && $scope.$on('$destroy', function () {
+                unsubscribe(name, fn);
+            });
+        }
+
+        function once(events, fn, $scope) {
+            validateCallback(fn);
+
+            if(events) {
+                if(typeof events == 'string') {
+                    validateName(events);
+
+                    var handler = function () {
+                        unsubscribe(events, handler);
+                        fn();
+                    };
+                    _subscribe(events, handler, $scope);
+                } else if(events instanceof Array) {
+                    if(!events.length) {
+                        throwNameError();
+                    }
+
+                    events.forEach(validateName);
+                    events.forEach(function (eventName) {
+                        var handler = function () {
+                            unsubscribe(eventName, handler);
+                            fn();
+                        };
+
+                        _subscribe(eventName, handler, $scope);
+                    });
+                } else {
+                    throwNameError();
+                }
+            } else {
+                throwNameError();
+            }
+
+            return this;
         }
 
         function fire(name, args) {
@@ -47,13 +97,23 @@
             subscriptions[name].forEach(function (fn) {
                 fn(args != undefined ? args : {}); // empty object can be used as a shared storage
             });
+
+            return this;
         }
 
         function unsubscribe(name, fn) {
             validateName(name);
             validateCallback(fn);
-            if (!subscriptions[name]) return true;
+            if (!subscriptions[name]) return;
             subscriptions[name].splice(subscriptions[name].indexOf(fn), 1);
+            return this;
+        }
+
+        function unsubscribeAll(name) {
+            validateName(name);
+            if (!subscriptions[name]) return;
+            subscriptions[name] = [];
+            return this;
         }
         
         function validateName(name) {
