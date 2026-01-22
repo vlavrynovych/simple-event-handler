a796aae6706ccb3787c6b22b3af98c522db5efe1
Volodymyr Lavrynovych <volodyalavrynovych@gmail.com>
2026-01-22 22:03:40 +0200

    build: replace Gulp with Rollup
    - Remove Gulp and related dependencies (del, gulp-concat, gulp-rename, gulp-sourcemaps, gulp-uglify)
    - Add Rollup and @rollup/plugin-terser
    - Create rollup.config.mjs with UMD format for both minified and unminified builds
    - Update build scripts to use Rollup instead of Gulp
    - Delete gulpfile.js
    - Reduce package count from 464 to 286 (38% reduction)

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+import terser from '@rollup/plugin-terser';
+import { readFileSync } from 'fs';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+const banner = `/**
+ * simple-event-handler v${pkg.version}
+ * (c) ${new Date().getFullYear()} Volodymyr Lavrynovych
+ * @license MIT
+ */`;
+
+export default [
+  // Unminified build
+  {
+    input: 'src/simple-event-handler.js',
+    output: {
+      file: 'dist/simple-event-handler.js',
+      format: 'umd',
+      name: 'EventHandler',
+      banner,
+      sourcemap: true,
+      exports: 'auto'
+    }
+  },
+  // Minified build
+  {
+    input: 'src/simple-event-handler.js',
+    output: {
+      file: 'dist/simple-event-handler.min.js',
+      format: 'umd',
+      name: 'EventHandler',
+      banner,
+      sourcemap: true,
+      exports: 'auto',
+      plugins: [terser({
+        format: {
+          comments: /^!/
+        }
+      })]
+    }
+  }
+];
